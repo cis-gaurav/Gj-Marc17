e4655c2659dc345b2070d612700802b0c78e5c0c
Gaurav <Gaurav@LAPTOP-U147QH6D>
2021-03-17 17:45:37 +0530

    Lets test

---
+++ GJM17/src/main/java/GJM17/GJM17/App.java
@@ -0,0 +1,13 @@
+package GJM17.GJM17;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ GJM17/src/test/java/GJM17/GJM17/M17.java
@@ -0,0 +1,29 @@
+package GJM17.GJM17;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class M17 {
+public static WebDriver driver;
+
+	
+	@BeforeTest
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver89\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void OpenBrowser() {
+		driver.get("https://mvnrepository.com/");
+	}
+	
+	@AfterTest
+	public void Teardown() {
+		driver.close();
+	}
+}
