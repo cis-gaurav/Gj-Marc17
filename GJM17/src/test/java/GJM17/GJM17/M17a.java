0894db14b552fcb239bc9329a7d7454166591182
Gaurav <Gaurav@LAPTOP-U147QH6D>
2021-03-18 16:57:39 +0530

    Add cases

--- GJM17/src/test/java/GJM17/GJM17/M17a.java
+++ GJM17/src/test/java/GJM17/GJM17/M17a.java
@@ -1,12 +1,13 @@
 package GJM17.GJM17;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class M17 {
+public class M17a {
 public static WebDriver driver;
 
 	
@@ -20,7 +21,9 @@ public static WebDriver driver;
 	@Test
 	public void OpenBrowser() {
 		driver.get("next.testmd.co.uk/");
-
+		driver.findElement(By.name("user[login]")).sendKeys("gaurav.j");
+		driver.findElement(By.name("user[password]")).sendKeys("123456789");
+		driver.findElement(By.name("commit")).click();	
 	}
 	
 	@AfterTest
